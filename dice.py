bb0050061c97d9631ca41779305831153a4f6ab4
mariswethz <43032088+mariswethz@users.noreply.github.com>
2018-09-22 10:31:30 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,13 @@
+dl310@soetcse:~/maria$ python3 lab42.py
+enter q to quit,or r for rolling the dicer
+6
+enter q to quit,or r for rolling the dicer
+4
+enter q to quit,or r for rolling the dicer
+1
+enter q to quit,or r for rolling the dicer
+1
+enter q to quit,or r for rolling the diceq
+byee..
+dl310@soetcse:~/maria$ 
+
